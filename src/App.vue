44d10030b79cbfcbb60202ff09f4dfc8271d5d54
matiassromo <matiasromo10@hotmail.com>
2024-10-16 17:05:31 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view></router-view> <!-- Aquí se renderizan las rutas -->
+  </div>
+</template>
+
+<script>
+import './style.css'; 
+
+export default {
+  name: 'App'
+};
+</script>
